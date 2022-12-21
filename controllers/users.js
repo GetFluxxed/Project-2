a4c7ca7597e854003b05ccf400b9cb3cbfd9d424
getfluxxed <derek@jondelconstruction.com>
2022-12-21 12:35:17 -0800

    added login/logout/signup logins

--- controllers/users.js
+++ controllers/users.js
@@ -32,5 +32,42 @@ router.post('/', async function (req, res) {
         res.status(500).send('server error')
     }
 })
+
+// GET /users/login -- render a login from that POSTs to /users/login
+router.get('/login', function (req, res) {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+// POST /users/login -- ingest data from form rendered @ GET /users/login
+router.post('/login', async function (req, res) {
+    try {
+        // look up the user based on their email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        // boilerplate message if login fails
+        const badCredentialMessage = 'username or password is incorrect'
+        if (!user) {
+            // if the user isnt found in the db
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else if (user.password !== req.body.password) {
+            // if the user's supplied password is incorrect
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else {
+            // if the user is found and their password matches log them in
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+// GET /users/logout -- clear any cookies and redirect to the homepage
+router.get('/logout', function (req, res) {
+    res.send('log the user out by clearing the cookie')
+})
 // export the router
 module.exports = router
